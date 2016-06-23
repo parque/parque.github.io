85ab9c66706faa7a4538304c99da0c584b581cd2
Kevin Mora <kevin.morag94@gmail.com>
2016-06-22 18:57:25 -0600

    bajando

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -24,7 +24,7 @@ var player;
 function onYouTubeIframeAPIReady() {
   player = new YT.Player('videoPrincipal', {
     videoId: '2CuLLqV_byM', // esto es el id del video que se encuentra en la url
-    playerVars: { 'autoplay': 0, 'controls': 1 ,'rel': 0, 'showinfo': 0 , 'controls': 2 , 'autohide' : 2},
+    playerVars: { 'autoplay': 0, 'controls': 1 ,'rel': 0, 'showinfo': 0, 'autohide' : 2},
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
